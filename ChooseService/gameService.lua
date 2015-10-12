213af473feb68bd9b359b391147c57b75b55c12c
ccmfeng <739414355@qq.com>
2015-10-12 22:00:18 +0800

    updae
    update

--- ChooseService/gameService.lua
+++ ChooseService/gameService.lua
@@ -4,97 +4,38 @@
 
 local skynet = require "skynet"
 local socket = require "socket"
-local httpd = require "http.httpd"
-local sockethelper = require "http.sockethelper"
-local urllib = require "http.url"
-local table = table
+
 local string = string
 
-local mode = ...
-
-if mode == "agent" then
-print("not agent 2")
-local function response(id, ...)
-	skynet.error("----------");
-	local ok, err = httpd.write_response(sockethelper.writefunc(id), ...)
-	if not ok then
-		-- if err == sockethelper.socket_error , that means socket closed.
-		skynet.error(string.format("fd = %d, %s", id, err))
-	end
-end
+--是否开启限制Http选服服务个数
+local isOpenLimitAgent = true
+local limitAgentNum = 20;
 
 skynet.start(function()
-	skynet.dispatch("lua", function (_,_,id)
-		socket.start(id)
-		-- limit request body size to 8192 (you can pass nil to unlimit)
-		local code, url, method, header, body = httpd.read_request(sockethelper.readfunc(id), 8192)
-		skynet.error("***********");
-		skynet.error(code)
-		skynet.error(url)
-		skynet.error(method)
-		skynet.error(header)
-		skynet.error("------------")
-		for k, v in pairs(header) do
-			local str = "key="..tostring(k).." value="..tostring(v)
-			skynet.error(str);
+	
+	local balance = 1
+	local serviceAgent = {};
+	
+	if isOpenLimitAgent then
+		for i= 1, limitAgentNum do
+			serviceAgent[i] = skynet.newservice("serviceAgent");
 		end
-		skynet.error("------------")
-		skynet.error(body);
-		skynet.error("***********");
-		if code then
-			if code ~= 200 then
-				response(id, code)
-			else
-				local tmp = {}
-				if header.host then
-					table.insert(tmp, string.format("host: %s", header.host))
-				end
-				local path, query = urllib.parse(url)
-				table.insert(tmp, string.format("path: %s", path))
-				if query then
-					local q = urllib.parse_query(query)
-					for k, v in pairs(q) do
-						table.insert(tmp, string.format("query: %s= %s", k,v))
-					end
-				end
-				table.insert(tmp, "-----header----")
-				for k,v in pairs(header) do
-					table.insert(tmp, string.format("%s = %s",k,v))
-				end
-				table.insert(tmp, "-----body----\n" .. body)
-				response(id, code, table.concat(tmp,"\n"))
+	end
+
+	skynet.error("监听游戏Http:8001端口")
+	local id = socket.listen("0.0.0.0", 8001)
+	socket.start(id , function(id, addr)
+
+		if isOpenLimitAgent then
+			skynet.error(string.format("%s connected, pass it to serviceAgent :%08x", addr, serviceAgent[balance]))
+			skynet.send(serviceAgent[balance], "lua", id, false);
+			balance = balance + 1
+			if balance > #serviceAgent then
+				balance = 1
 			end
 		else
-			if url == sockethelper.socket_error then
-				skynet.error("socket closed")
-			else
-				skynet.error(url)
-			end
-		end
-		socket.close(id)
-	end)
-end)
-
-else
-
-print("not agent 1")
-
-skynet.start(function()
-	local agent = {}
-	for i= 1, 20 do
-		agent[i] = skynet.newservice(SERVICE_NAME, "agent")
-	end
-	local balance = 1
-	local id = socket.listen("0.0.0.0", 8001)
-	skynet.error("Listen web port 8001")
-	socket.start(id , function(id, addr)
-		skynet.error(string.format("%s connected, pass it to agent :%08x", addr, agent[balance]))
-		skynet.send(agent[balance], "lua", id)
-		balance = balance + 1
-		if balance > #agent then
-			balance = 1
+			local Agent = skynet.newservice("serviceAgent");
+			skynet.send(Agent, "lua", id, true);
 		end
 	end)
 end)
-
-end
---
+++ ChooseService/serviceAgent.lua
@@ -0,0 +1,77 @@
+
+-- 游戏选服列表服务
+-- 一个Http服务,用于选服列表
+
+local skynet = require "skynet"
+local socket = require "socket"
+local httpd = require "http.httpd"
+local sockethelper = require "http.sockethelper"
+local urllib = require "http.url"
+local table = table
+local string = string
+
+
+
+local function response(id, ...)
+	skynet.error("----------");
+	local ok, err = httpd.write_response(sockethelper.writefunc(id), ...)
+	if not ok then
+		-- if err == sockethelper.socket_error , that means socket closed.
+		skynet.error(string.format("fd = %d, %s", id, err))
+	end
+end
+
+skynet.start(function()
+	skynet.dispatch("lua", function (_,_,id)
+		socket.start(id)
+		-- limit request body size to 8192 (you can pass nil to unlimit)
+		local code, url, method, header, body = httpd.read_request(sockethelper.readfunc(id), 8192)
+		skynet.error("***********");
+		skynet.error(code)
+		skynet.error(url)
+		skynet.error(method)
+		skynet.error(header)
+		skynet.error("------------")
+		for k, v in pairs(header) do
+			local str = "key="..tostring(k).." value="..tostring(v)
+			skynet.error(str);
+		end
+		skynet.error("------------")
+		skynet.error(body);
+		skynet.error("***********");
+		if code then
+			if code ~= 200 then
+				response(id, code)
+			else
+				local tmp = {}
+				if header.host then
+					table.insert(tmp, string.format("host: %s", header.host))
+				end
+				local path, query = urllib.parse(url)
+				table.insert(tmp, string.format("path: %s", path))
+				if query then
+					local q = urllib.parse_query(query)
+					for k, v in pairs(q) do
+						table.insert(tmp, string.format("query: %s= %s", k,v))
+					end
+				end
+				table.insert(tmp, "-----header----")
+				for k,v in pairs(header) do
+					table.insert(tmp, string.format("%s = %s",k,v))
+				end
+				table.insert(tmp, "-----body----\n" .. body)
+				response(id, code, table.concat(tmp,"\n"))
+			end
+		else
+			if url == sockethelper.socket_error then
+				skynet.error("socket closed")
+			else
+				skynet.error(url)
+			end
+		end
+		socket.close(id)
+		
+		skynet.exit()
+	end)
+end)
+
